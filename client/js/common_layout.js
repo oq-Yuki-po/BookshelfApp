83cc49d494ed13af9d7a1a94284a2a84cf2f99e2
oq-Yuki-po <o.yukinko0715@gmail.com>
2019-06-09 12:44:17 +0900

    BA-001 共通ヘッダ作成

---
+++ client/js/common_layout.js
@@ -0,0 +1,14 @@
+function header() {
+    var html = "";
+    html += '<header>';
+    html += '<h1>BookShelf</h1>';
+    html += '    <nav>';
+    html += '    <ul>';
+    html += '        <li><a href="index.html">Top</a></li>';
+    html += '        <li><a href="registration.html">SignUp</a></li>';
+    html += '        <li><a href="search.html">Search</a></li>';
+    html += '    </ul>';
+    html += '</nav>   ';
+    html += '</header>';
+    document.write(html);
+}
