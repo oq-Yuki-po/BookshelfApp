c46b640fb8f98970da45a40d8167d4d3ef49a7ab
oq-Yuki-po <o.yukinko0715@gmail.com>
2019-06-30 21:17:58 +0900

    BA-008 top画面削除

--- client/js/common_layout.js
+++ client/js/common_layout.js
@@ -4,7 +4,7 @@ function header() {
     html += '<h1>BookShelf</h1>';
     html += '    <nav>';
     html += '    <ul>';
-    html += '        <li><a href="top.html">Top</a></li>';
+    // html += '        <li><a href="top.html">Top</a></li>';
     html += '        <li><a href="book_register.html">Register</a></li>';
     html += '        <li><a href="book_search.html">Search</a></li>';
     html += '    </ul>';
