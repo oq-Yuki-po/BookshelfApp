0bd3babd4eba4a6faca3a5fe895633684dd2f449
oq-Yuki-po <o.yukinko0715@gmail.com>
2019-07-06 23:59:54 +0900

    BA-008 レイアウト修正 登録機能を検索画面に移動

--- client/js/books.js
+++ client/js/books.js
@@ -1,5 +1,9 @@
-document.getElementById('search').onclick = function(){
+document.getElementById('search').onclick = function () {
   search();
+}
+
+document.getElementById('register').onclick = function () {
+  register();
 }
 
 function search() {
@@ -16,13 +20,28 @@ function search() {
   xhttp.send();
 }
 
+function register() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText)
+    }
+  };
+  const isbn = document.getElementById('isbn').value;
+  xhttp.open("POST", "/api/books", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(`isbn=${isbn}`);
+}
+
 function make_table(json) {
   document.getElementById("search_result").textContent = null;
   for (var i = 0; i < json.length; i++) {
+    var book_title = json[i]['title'];
+    var book_author = json[i]['name'];
     var div = document.createElement("div");
-    div.classList.add("col-5");
+    div.classList.add("panel");
     var title = document.createElement("h6");
-    title.innerText = `${json[i]['title']}`;
+    title.innerText = book_title;
     title.classList.add('title');
     div.appendChild(title);
     var img = document.createElement('img');
@@ -32,7 +51,20 @@ function make_table(json) {
       img.src = `/img/no_img.png`;
     }
     img.classList.add('cover');
+    img.addEventListener('click', function () {
+      display_book_info(book_title, book_author);
+    },
+      false);
     div.appendChild(img);
     document.getElementById("search_result").appendChild(div);
   }
+  document.getElementById("search_result").classList.add('search_result');
+}
+
+function display_book_info(title, author) {
+  Swal.fire({
+    html: `${title}<br>${author}`,
+    type: 'info',
+    width: '64rem'
+  })
 }
